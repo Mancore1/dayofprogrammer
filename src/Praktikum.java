b914de327046743fb3fd8f46d943434abc07b5a2
Vlad <fairgvard@yandex.ru>
2022-11-10 01:30:10 +0300

    initial commit

---
+++ src/Praktikum.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Praktikum {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите год:");
+        int year = scanner.nextInt();
+
+        if (isLeapYear(year)) {
+            System.out.printf("12.09.%s", year);
+        } else {
+            System.out.printf("13.09.%s", year);
+        }
+    }
+
+    public static boolean isLeapYear(int year) {
+        return year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
+    }
+}
+
+
